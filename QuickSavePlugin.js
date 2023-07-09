83a4655c62352ef320d6e504e71c501b10ceb966
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2023-07-09 13:01:54 +0300

    infra: add Source slice (for EEP, etc)

--- QuickSavePlugin.js
+++ QuickSavePlugin.js
@@ -2,6 +2,7 @@
 |Description|Enables Ctrl + S hotkey to save changes (and not browser default action)|
 |Version    |1.1|
 |Author     |Yakov Litvin|
+|Source     |https://github.com/YakovL/TiddlyWiki_QuickSavePlugin/blob/master/QuickSavePlugin.js|
 ***/
 //{{{
 jQuery(document).on('keydown', null, function($e) {
